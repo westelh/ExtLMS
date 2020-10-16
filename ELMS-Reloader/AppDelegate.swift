a8c31d96cade83ba2e9cd0c314e45382a1f1076c
westelh <westelh@gmail.com>
2020-10-16 23:44:55 +0900

    Initial Commit

---
+++ ELMS-Reloader/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ELMS-Reloader
+//
+//  Created by 西海泰成 on 2020/10/16.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+}
---
+++ ELMS-Reloader/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  ELMS-Reloader
+//
+//  Created by 西海泰成 on 2020/10/16.
+//
+
+import Cocoa
+import SafariServices.SFSafariApplication
+import SafariServices.SFSafariExtensionManager
+
+let appName = "ELMS-Reloader"
+let extensionBundleIdentifier = "io.github.westelh.ELMS-Reloader-Extension"
+
+class ViewController: NSViewController {
+
+    @IBOutlet var appNameLabel: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.appNameLabel.stringValue = appName
+        SFSafariExtensionManager.getStateOfSafariExtension(withIdentifier: extensionBundleIdentifier) { (state, error) in
+            guard let state = state, error == nil else {
+                // Insert code to inform the user that something went wrong.
+                return
+            }
+
+            DispatchQueue.main.async {
+                if (state.isEnabled) {
+                    self.appNameLabel.stringValue = "\(appName)'s extension is currently on."
+                } else {
+                    self.appNameLabel.stringValue = "\(appName)'s extension is currently off. You can turn it on in Safari Extensions preferences."
+                }
+            }
+        }
+    }
+    
+    @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
+        SFSafariApplication.showPreferencesForExtension(withIdentifier: extensionBundleIdentifier) { error in
+            guard error == nil else {
+                // Insert code to inform the user that something went wrong.
+                return
+            }
+
+            DispatchQueue.main.async {
+                NSApplication.shared.terminate(nil)
+            }
+        }
+    }
+
+}
