5132b6ec5b825f7a0f95d44c4c2e50b243b439b8
westelh <westelh@gmail.com>
2020-10-18 09:15:42 +0900

    When session is out, extension now reloads login page.

--- ELMS-Reloader Extension/Resources/content.js
+++ ELMS-Reloader Extension/Resources/content.js
@@ -1,41 +1,49 @@
-function requestIdentifier(name, resolve) {
-    const request = { requestType:"Identifier", "name":name}
-    return browser.runtime.sendMessage(request).then(response => {
-        console.log("Got a response", response)
-        if(response?.error) console.error("Error from background:", response?.error)
-        return response?.value
-    }).then(resolve)
+function checkIfTimeout() {
+    const title = document.getElementsByTagName('h1')
+    if(title?.innerHTML == "タイムアウト") {
+        return true
+    } else {
+        return false
+    }
 }
 
-const userPromise = requestIdentifier("User", (value) => {
-    var pass_input = document.getElementById('username_input')
-    if(pass_input) {
-        pass_input.value = value
-        return value
-    } else {
-        console.warn('User input not found.')
+if(checkIfTimeout()) {
+    location.href = "https://www.elms.hokudai.ac.jp/"
+} else {
+    function requestIdentifier(name, resolve) {
+        const request = { requestType:"Identifier", "name":name}
+        return browser.runtime.sendMessage(request).then(response => {
+            console.log("Got a response", response)
+            if(response?.error) console.error("Error from background:", response?.error)
+            return response?.value
+        }).then(resolve)
     }
-});
 
-const passPromise = requestIdentifier("Pass", (value) => {
-    var pass_input = document.getElementById('password_input')
-    if(pass_input) {
-        pass_input.value = value
-        return value
-    } else {
-        console.warn('Password input not found.')
-    }
-});
+    const userPromise = requestIdentifier("User", (value) => {
+        var pass_input = document.getElementById('username_input')
+        if(pass_input) {
+            pass_input.value = value
+            return value
+        } else {
+            console.warn('User input not found.')
+        }
+    });
 
-if(false) {
-    // TODO
-    // セッション切れの場合リロード
+    const passPromise = requestIdentifier("Pass", (value) => {
+        var pass_input = document.getElementById('password_input')
+        if(pass_input) {
+            pass_input.value = value
+            return value
+        } else {
+            console.warn('Password input not found.')
+        }
+    });
+    
+    Promise.all([userPromise, passPromise]).then((user, pass) => {
+        var button = document.getElementById('login_button')
+        var error_message = document.getElementById('error_message')
+        if(!error_message) {
+            button?.click()
+        }
+    });
 }
-
-Promise.all([userPromise, passPromise]).then((user, pass) => {
-    var button = document.getElementById('login_button')
-    var error_message = document.getElementById('error_message')
-    if(!error_message) {
-        button?.click()
-    }
-});
