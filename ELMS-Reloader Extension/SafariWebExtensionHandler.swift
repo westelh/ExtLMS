e0d35851da92d2dc80ceb1b5f9277728428ccc19
westelh <westelh@gmail.com>
2020-10-22 11:05:59 +0900

    Fix a fatal crash

--- ELMS-Reloader Extension/SafariWebExtensionHandler.swift
+++ ELMS-Reloader Extension/SafariWebExtensionHandler.swift
@@ -16,41 +16,31 @@ let PassKeyInDefault = "Pass"
 
 class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
     let defaults = UserDefaults(suiteName: "io.github.westelh.ELMS-Reloader.group")
-    // On MacOS10.15 with Xcode12.0.1, having a custom OSLog and logging with it leads app crash.
-    // Instead of using custom log, use OSLog.default
-    // let logger = OSLog(subsystem: "io.github.westelh.ELMS-Reloader", category: "SafariExtensionNativeContainer")
-    
-    //定義済みメッセージタイプとそれに対するアクション
-    let messageTypesAndDelegates = [
-        "request":meetRequest
-    ]
-    
-    func respondToBrowserMessage(_ message: [AnyHashable:Any]) -> [AnyHashable:Any]? {
-        os_log("Entered respondToBrowsermessage()", log: OSLog.default, type: .debug)
-        // 定義済みメッセージタイプにマッチするメッセージ内容を走査
-        for (messageType, delegate) in messageTypesAndDelegates {
-//            os_log("Message type enumeration started. now:%@", log: logger, type: .debug, messageType)
-            if let contentOfType = message[messageType] {
-//                os_log("Received message has explicit type \"%@\". Passing to the delegate corresponds.", log: logger, type: .info, messageType)
-                return delegate(self)(contentOfType as? String ?? "")
-            }
-        }
-//        os_log("Received message does not match any type of message defined.", log: logger, type: .error)
-        return nil
-    }
+    let logger = OSLog(subsystem: "io.github.westelh.ELMS-Reloader", category: "SafariExtensionNativeContainer")
     
 	func beginRequest(with context: NSExtensionContext) {
         let item = context.inputItems.first as! NSExtensionItem
-        
         var response: [AnyHashable:Any]?
         if let message = item.userInfo?[SFExtensionMessageKey] as? [AnyHashable:Any] {
-            // os_log("Received message from browser.runtime.sendMesssage()", log: OSLog.default, type: .default)
+            os_log("Received message from browser.runtime.sendMesssage()", log: logger, type: .default)
             response = respondToBrowserMessage(message)
         } else {
-//            os_log("No data in the location expected for it to be in", log: OSLog.default, type: .error)
+            os_log("No data in the location expected for it to be in", log: logger, type: .error)
         }
         
-        context.completeRequest(returningItems: [response ?? [] ], completionHandler: nil)
+        let ret = NSExtensionItem()
+        ret.userInfo?.updateValue(response ?? [:], forKey: SFExtensionMessageKey)
+        context.completeRequest(returningItems: [ret], completionHandler: nil)
+    }
+    
+    func respondToBrowserMessage(_ message: [AnyHashable:Any]) -> [AnyHashable:Any]? {
+        if let requestContent = message["request"] as? String {
+            os_log("At subroutine switch, message content(%{public}@) is being passed to request process.", log: logger, type: .info, requestContent)
+            return meetRequest(requestContent: requestContent)
+        }
+
+        os_log("Received message does not match any type of message defined.", log: logger, type: .error)
+        return nil
     }
     
     func meetRequest(requestContent: String) -> [AnyHashable:Any] {
@@ -62,7 +52,7 @@ class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
             content = ["value":defaults?.string(forKey: PassKeyInDefault) ?? ""]; break
         default:
             content = ["error":String(format: "No operation for the request content:%@", requestContent)]
-//            os_log("No operation for the request content:%{public}@", log: OSLog.default, type: .error, requestContent)
+            os_log("No operation for the request content:%{public}@", log: logger, type: .error, requestContent)
         }
         return content
     }
